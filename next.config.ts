307e171a73ba7799d414cd32154bc1a6f4c7aa86
Vucongchien <vucongchien204@gmail.com>
2026-08-05 08:24:05 +0700

    📦️

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  cacheComponents: true,
   allowedDevOrigins: ["127.0.0.1"],
   experimental: {
     useTypeScriptCli: true,
